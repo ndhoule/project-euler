483a0ae992a8ee612a6094654b4c2c8627a87471
Nathan Houle <nathan@nathanhoule.com>
2014-08-11 13:50:53 -0700

    Remove inline tests in favor of dedicated test files

--- clojure/src/euler/001.clj
+++ clojure/src/euler/001.clj
@@ -15,21 +15,13 @@
 (defn multiples-3-and-5-sol1 [min max]
   (reduce + (filter multiple-of-3-or-5? (range min max))))
 
-(is (= 233168 (multiples-3-and-5-sol1 0 1000)))
-
-
 ; Use `distinct` instead
 (defn multiples-3-and-5-sol2 [min max]
   (reduce + (distinct (concat (range min max 3)
                               (range min max 5)))))
-
-(is (= 233168 (multiples-3-and-5-sol2 0 1000)))
-
 
 ; Produce two ranges and convert to `set` to deduplicate the range
 ; Apparently slightly more performant (but who cares)
 (defn multiples-3-and-5-sol3 [min max]
   (reduce + (set (concat (range min max 3)
                          (range min max 5)))))
-
-(is (= 233168 (multiples-3-and-5-sol3 0 1000)))
